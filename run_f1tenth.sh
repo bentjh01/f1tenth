1a2337e02904bacd6e237bce0f3665f3521bb8bf
Benjamin Teh <benjamintehjh01@gmail.com>
2024-10-27 21:17:44 +0800

    update

--- run_f1tenth.sh
+++ run_f1tenth.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-echo "Starting F1TENTH Container"
-
 if [ "$(docker ps -aq -f status=running -f name=${container_name})" ]
 then
 	echo "Container is Running. Starting new session."
 	docker exec -it "f1tenth_bt" bash 
 else
-	echo "Creating new container."
-
+    echo "Starting F1TENTH Container"
     docker run -it --rm \
     --name "f1tenth_bt" \
     -h "f1tenth_bt" \
