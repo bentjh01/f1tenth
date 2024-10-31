e932761caa3da3b2a43389324326c19abf390431
Benjamin Teh <benjamintehjh01@gmail.com>
2024-10-31 10:53:00 +0800

    refactor

--- run_f1tenth.sh
+++ run_f1tenth.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+container_name="f1tenth_bt"
+
+function create_container {
+    docker run -it --rm \
+        --name $container_name \
+        -h "f1tenth_bt" \
+        --network=host \
+        --volume="$(pwd)/f1tenth_ws":"/root/f1tenth_ws" \
+        bentjh01:f1tenth-foxy
+}
+
 if [ "$(docker ps -aq -f status=running -f name=${container_name})" ]
 then
 	echo "Container is Running. Starting new session."
-	docker exec -it "f1tenth_bt" bash 
+	docker exec -it ${container_name} bash 
 else
-    echo "Starting F1TENTH Container"
-    docker run -it --rm \
-    --name "f1tenth_bt" \
-    -h "f1tenth_bt" \
-    --network=host \
-	--volume="$(pwd)/f1tenth_ws":"/root/f1tenth_ws" \
-    bentjh01:f1tenth-foxy
+    echo "Starting container"
+    create_container
 fi
